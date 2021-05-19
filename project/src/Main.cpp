85572c9d5b25662b72e89301ec4341159f2077e3
NADEE-MJ <nadeem.maida@gmail.com>
2021-05-19 07:18:16 -0700

    added testing blocks for displayHand method

--- project/src/Main.cpp
+++ project/src/Main.cpp
@@ -22,7 +22,14 @@ int main() {
     d.shuffle();
     std::cout << "Game: Blackjack" << std::endl << std::endl;
     std::cout << "Here are your cards:" << std::endl;
-    
+    player.hand[0].value = 9;
+    player.hand[0].suit = 'D';
+
+    player.hand[1].value = 10;
+    player.hand[1].suit = 'D';
+
+    player.handValue();
+    player.displayHand();
     player.dealHand(d.deck, d.topCard);
     player.handValue();
     player.displayHand();
